8574af3e9bf6f4b29ef4932e42bc5002c57bc6fe
fishingfly <zhouyuzf@163.com>
2017-06-28 09:29:51 +0800

    updata a sending function
    learning something

--- SpeedGuidance.h
+++ SpeedGuidance.h
@@ -16,6 +16,7 @@ class MainWindow : public QMainWindow
 public:
     explicit MainWindow(QWidget *parent = 0);
     ~MainWindow();
+    void send();
     void judge(int light_status,float lefttime,float current_speed,float limit_speed,float distance_toIS,float greenlight_time);//引导函数
     int Intersection(float L,float R,int ID,int LastID);//判断是否需要引导
     float Cal_distance(float latitude1,float longitude1,float latitude2,float longitude2);//计算距离
